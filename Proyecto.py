87afdf1adaeffaae0b094b368edf02fd4d80e60c
unknown <sergioandres36@gmail.com>
2024-02-09 20:44:23 +0100

    intento de subir

---
+++ Proyecto.py
@@ -0,0 +1,4 @@
+nombre = input('Por favor, ingresa tu nombre: ')
+ventas = float(input('dime cuando vendiste este mes: '))
+comision = ventas * 0.18
+print('vendiste ' + str(ventas) + ' ,por lo que tu comision ' + str(comision) )
---
+++ ejercicios.py
@@ -0,0 +1,90 @@
+# 1
+
+nombre = 'Sergio'
+edad = 30
+
+# 2 
+
+nombre = 'Sergio'
+Apellido = 'Grisales'
+nombre_completo = (nombre + ' ' +Apellido)
+
+# 3 
+libro = 'Python'
+print('Estoy aprendiendo ' + libro + ' gracias a este libro de libro')
+
+# 4 
+num_entero = 23
+print(num_entero)
+
+# 5 
+num_decimal = 2.3
+print(num_decimal)
+
+# 6 
+num1 = 7.5
+num2 = 2.5
+suma = num1 + num2
+print(suma)
+
+# 7 
+num1 = 7.5
+num2 = 2.5
+num1_int = int(num1)
+print(num1_int)
+
+# 8 
+num1 = 7.5
+num2 = 2
+num2_float = float(num2)
+print(num2_float)
+
+# 9 
+num1 = 7.5
+num2 = 2.5
+print(num1 + num2)
+
+# 10
+nombre_asociado = 'Sergio'
+numero_asociado = '123456789'
+texto = 'Estimado ' + nombre_asociado + ' su número de asociado es: ' + numero_asociado
+print (texto)
+
+# 11
+puntos_nuevos = 500
+puntos_totales = 1000
+texto2 = ('Has ganado ' + str(puntos_nuevos) + ' puntos! En total, acumulas ' + str(puntos_totales) + ' puntos')
+print(texto2)
+
+# 12
+totales = puntos_totales + puntos_nuevos
+print('Has ganado ' + str(puntos_nuevos) + ' puntos! En total, acumulas ' + str(totales) + ' puntos')
+
+# 13
+numero_1 = 874
+numero_2 = 27
+dividir = numero_1 / numero_2
+print(dividir)
+
+# 14 
+numero_1 = 456
+numero_2 = 33
+dividir = numero_1 % numero_2
+print(dividir)
+
+# 15
+print(783 **0.5)
+
+# 16 
+divicion = 10 / 3
+resueltado = round(divicion, 2)
+print(resueltado)
+
+# 17
+numero = 10.676767
+redondeo = round(numero)
+print(redondeo)
+# 18 
+numero = 5**0.5
+redondeo = round(numero, 3)
+print(redondeo)
